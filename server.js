a9783ec51ab06e6d0a4446f96d0cfe064b0f872e
JithinAntony4 <jithinantony333@gmail.com>
2017-02-04 14:56:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/jithin.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'jithin.png'));
+app.get('/ui/rg.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'rg.png'));
 });
 
 
