544edb2f23bcd8543fdd098381966ce4769cc204
Jithin Antony <jithinantony333@gmail.com>
2017-02-04 19:01:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = "Changed value here..";
+//move the image 
+var img = document.getElementById('jca');
+img.onClick = function(){
+  img.style.marginLeft = "100px"  ;
+};
