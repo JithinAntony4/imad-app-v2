f2ff8af2d05625696ab7b4245b768477c62d1b63
Jithin Antony <jithinantony333@gmail.com>
2017-02-04 17:13:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,36 @@ function createTemplate(data){
     var heading = data.heading;
     var date = data.date;
     var content = data.content;
-    
+    var htmlTemplate = `
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport cotent=" width="device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>`
+;
+ return htmlTemplate;  
 }
 
 app.get('/article-one', function(req,res){
