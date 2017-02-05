40030051def992f60be2ada4496e3e74d1392d91
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 17:58:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,37 +23,6 @@ button.onclick = function(){
     request.send(null);
 };
 
-//Submit comment
-var commentbtn = document.getElementById('submit_comment_btn');
-commentbtn.onclick = function() {
-    //Create a request object
-    var request = new XMLHttpRequest();
-    //capture the variable and store it on variable
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          //Take action
-          //200 means request is successfully completed
-          if (request.status === 200) {
-            //Capture a list of comment and render it as list
-            var comments = request.responseText;
-            comments = JSON.parse(comments);
-            var list = '';
-            for (var i = 0; i < comments.length; i++) {
-                list += '<li>' + comments[i] + '</li>';
-            }
-            var ul = document.getElementById('comment_list');
-            ul.innerHTML = list;
-          }
-      }
-      //Not done yet
-    };
-    //Make a request
-    var commentInput = document.getElementById('comment_input');
-    var comment = commentInput.value;
-    request.open('GET', 'http://jithinantony4.imad.hasura-app.io/submit-comment?comment=' + comment, true);
-    request.send(null);
-};
-
 //Submit name
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
@@ -83,5 +52,36 @@ submit.onclick = function() {
     var nameInput = document.getElementById('name_input');
     var name = nameInput.value;
     request.open('GET', 'http://jithinantony4.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
+
+//Submit comment
+var commentbtn = document.getElementById('submit_comment_btn');
+commentbtn.onclick = function() {
+    //Create a request object
+    var request = new XMLHttpRequest();
+    //capture the variable and store it on variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //Take action
+          //200 means request is successfully completed
+          if (request.status === 200) {
+            //Capture a list of comment and render it as list
+            var comments = request.responseText;
+            comments = JSON.parse(comments);
+            var list = '';
+            for (var i = 0; i < comments.length; i++) {
+                list += '<li>' + comments[i] + '</li>';
+            }
+            var ul = document.getElementById('comment_list');
+            ul.innerHTML = list;
+          }
+      }
+      //Not done yet
+    };
+    //Make a request
+    var commentInput = document.getElementById('comment_input');
+    var comment = commentInput.value;
+    request.open('GET', 'http://jithinantony4.imad.hasura-app.io/submit-comment?comment=' + comment, true);
     request.send(null);
 };
