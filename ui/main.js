26832431ed383719d4cadba8130a338134f00e75
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 09:54:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,6 @@
-console.log('Loaded!');
-//change the text of the main-text div
-//var element = document.getElementById('main-text');
-//element.innerHTML = "Changed value here..";
-//move the image 
-/*var img = document.getElementById('jca');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-     img.style.marginLeft = marginLeft + "px";
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-};*/
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function(){
+    
+    //make request counter end point
+};
