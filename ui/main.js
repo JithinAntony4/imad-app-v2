7606af4a2d3649f09cf7e7d1d90df9ccbe8f25bd
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 09:54:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,6 @@ var button = document.getElementById('counter');
 button.onclick = function(){
     
     //make request counter end point
+    
+    //capture the variable and store it on variable
 };
