abedc44ec875617a1892e676831e0b3c6d9a1323
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 18:14:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,12 +110,7 @@ app.get('/:articleName', function(req,res){
     //articleName == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
-    switch (articleName.toString()) {
-        case "article-one":
-            res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-            break;
-    }
-    //res.send(createTemplate(articles[articleName]));
+    res.send(createTemplate(articles[articleName]));
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
