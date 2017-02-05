549b4847bffea417b83ff85c8de2a271263c6e64
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 10:24:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ button.onclick = function(){
       if (request.readyState === XMLHttpRequest.DONE) {
           //Take action
           //200 means request is successfully completed
-          if (request === 200) {
+          if (request.status === 200) {
               var counter = request.responseText;
               var span = document.getElementById('count');
               span.innerHTML = counter.toString();
