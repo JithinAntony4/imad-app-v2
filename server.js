9cf47c69980bc1c076f351da37444cf3cf69ee60
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 11:15:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/ui/jithin.jpg', function (req, res) {
 var names = [];
 app.get('/submit-name', function (req, res) { //URL : ../submit-name?name=xxxx
     //get the name from request
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     //JSON JavaScript Object Notation
     res.send(JSON.stringify(names)); 
