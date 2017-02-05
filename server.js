3f2315adb7c11760455b04dbef54f9481437285e
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 18:21:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,6 +123,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/comment.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/jithin.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'jithin.jpg'));
 });
