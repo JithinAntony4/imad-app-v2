f1a600a7ad731eddb7dcf59170001cad5cad27b4
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 11:53:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,3 +53,33 @@ submit.onclick = function() {
     request.open('GET', 'http://jithinantony4.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
+//Submit comment
+var comment_btn = document.getElementById('submit_comment_btn')
+comment_btn.onclick = function() {
+    //Create a request object
+    var request = new XMLHttpRequest();
+    //capture the variable and store it on variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //Take action
+          //200 means request is successfully completed
+          if (request.status === 200) {
+            //Capture a list of comment and render it as list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for (var i = 0; i < names.length; i++) {
+                list += '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('name_comment_list');
+            ul.innerHTML = list;
+          }
+      }
+      //Not done yet
+    };
+    //Make a request
+    var nameInput = document.getElementById('name_input');
+    var name = nameInput.value;
+    request.open('GET', 'http://jithinantony4.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
