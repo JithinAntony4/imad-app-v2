d1bc984eae5a39a146923f19c94aff3ef5555573
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 11:02:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,6 +100,10 @@ app.get('/ui/jithin.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'jithin.jpg'));
 });
 
+app.get('/submit-name', function (req, res) {
+    
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
