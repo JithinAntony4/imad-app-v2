bf69bf2cb484bb4694a1a8a9f730ee3e7fcdd72f
Jithin Antony <jithinantony333@gmail.com>
2017-02-05 12:43:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ function createTemplate(data){
         <div class='footer'>
             <input type="text" id="comment_input" placeholder="Enter your comment here.." />
             <input type="submit" value="Submit" id="submit_comment_btn" >
-            <a href="/submit-comment"</a>
+            <a href="/submit-comment"></a>
             </input>
             <ul id="comment_list">
                 
