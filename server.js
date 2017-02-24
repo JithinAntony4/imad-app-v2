7dfcfdb41d22dde4c24d7a5490b5f73ec04c96c5
Jithin Antony <jithinantony333@gmail.com>
2017-02-24 14:08:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,9 +121,14 @@ app.get('/:articleName', function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool = new pg.Pool(config);
 app.get('/test-db', function (req, res){
     //make a select request
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+    });
     //return a response with results
 });
 
