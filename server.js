58426a3bdc6a45e0f348b426d817290d5add09a1
Jithin Antony <jithinantony333@gmail.com>
2017-02-24 13:50:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,6 +117,7 @@ app.get('/', function (req, res) {
 
 app.get('/test-db', function (req, res){
     //make a select request
+    //return a response with results
 });
 
 app.get('/ui/style.css', function (req, res) {
