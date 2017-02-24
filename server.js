63966ed1065f123c56e4900b4fb47ccda27a7a0f
Jithin Antony <jithinantony333@gmail.com>
2017-02-24 13:49:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res){
+app.get('/test-db', function (req, res){
     
 });
 
