9deda3d201f218be2453c734fe35154e1a1abdf6
Jithin Antony <jithinantony333@gmail.com>
2017-03-18 10:43:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function hash(input, salt){
 app.get('login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
-    Pool.query('SELECT * FROM "user" WHERE username=$1',[username], function(err, result){
+ool.query('SELECT * FROM "user" WHERE username=$1',[username], function(err, result){
         if(err){
             res.status(500).send(err.toString());
         } else {
@@ -50,7 +50,7 @@ app.post('/create-user', function(req, res){
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
-    Pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString], function(err, result){
+ool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString], function(err, result){
         if(err){
             res.status(500).send(err.toString());
         } else {
