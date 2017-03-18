befd20004470f0478e8753bef3c93de136bed28a
Jithin Antony <jithinantony333@gmail.com>
2017-03-18 11:01:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function hash(input, salt){
     return ['pbkdf2',"10000", salt, hashed.toString('hex')].join('$');
 }
 
-app.get('login', function(req, res){
+app.post('/login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
     pool.query('SELECT * FROM "user" WHERE username=$1',[username], function(err, result){
