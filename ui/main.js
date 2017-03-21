98bb91422768b707ae0544aa016bed46f460e057
Jithin Antony <jithinantony333@gmail.com>
2017-03-21 10:44:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,6 +34,18 @@ function loadLoginForm () {
           }  
           // Not done yet
         };
+                
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
 //Submit name
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
