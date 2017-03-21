c47ac4fb005664455165fca8aa419c4c6e9ecd28
Jithin Antony <jithinantony333@gmail.com>
2017-03-21 10:40:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,3 @@
-//counter code
-var button = document.getElementById('counter');
-button.onclick = function(){
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    //capture the variable and store it on variable
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          //Take action
-          //200 means request is successfully completed
-          if (request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
-      }
-      //Not done yet
-    };
-    //Make a request
-    request.open('GET', 'http://jithinantony4.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
 
 //Submit name
 var submit = document.getElementById('submit_btn');
